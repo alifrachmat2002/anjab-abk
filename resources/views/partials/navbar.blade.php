fd60905beb1344dd695e1c44a1aba15e2f2cbd42
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-08-28 13:43:26 +0700

    chore: add route to navbar button 'jabatan'

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -47,7 +47,7 @@
                         </li>
                         <li><a class="nav-link" href="{{ route('admin.tugas-tambahan.index') }}"><img data-feather="edit" width="20px">Tugas
                                 Tambahan</a></li>
-                        <li><a class="nav-link" href=""><img data-feather="file"
+                        <li><a class="nav-link" href="{{ route('admin.jabatan.index') }}"><img data-feather="file"
                                     width="20px">Jabatan</a></li>
                         <li><a class="nav-link" href=""><img data-feather="home" width="20px">Unsur</a>
                         </li>
